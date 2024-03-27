79a21c88549d069ca2626a9416c96d0daca40106
momal7 <163012470+momal7@users.noreply.github.com>
2024-03-27 15:20:00 +0500

    Add files via upload

---
+++ cli_calculator.ts
@@ -0,0 +1,53 @@
+// Assignment of calculator
+
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt([
+
+
+    {
+
+        name : "Num_1",
+        message : "Enter 1st Number: ",
+        type : "number"
+
+    },
+    {
+        name : "Num_2",
+        message : "Enter 2nd number",
+        type : "number"
+    },
+    {
+        name : "operator",
+        message : "select the operator" ,
+        type : "list",
+        choices: [ 'Add' , 'subtract' , 'Multiply' , 'Divid' ]
+    },
+]);
+let result
+switch (answer.operator) {
+
+    case 'Add':
+        result = answer.Num_1 + answer.Num_2
+        console.log("The answer of the addition is :" , result);
+        break;
+
+     case 'subtract':
+            result = answer.Num_1 - answer.Num_2
+            console.log("The answer of the subtraction is :", result);
+    
+       break;
+
+     case 'Multiply':
+                result = answer.Num_1 * answer.Num_2
+                console.log("The answer of the multiply is :",result);
+       break;
+
+     case 'Divide':
+                    result = answer.Num_1 / answer.Num_2
+                    console.log("The answer of the divid is :", result);
+       break;
+}
+
+
+
